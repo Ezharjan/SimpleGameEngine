01f3c48baaa9625bb6f152b839936ed51ebc0dce
Unknown_Man <2872953035@qq.com>
2020-07-03 09:54:15 +0800

    basicmove

--- src/game.ts
+++ src/game.ts
@@ -44,42 +44,10 @@ core.registerBehaviourClass(KeyContainerBehaviour)
 
 class GameStartupBehaviour extends Behaviour {
     onStart() {
-<<<<<<< HEAD
-=======
-        this.warrior = core.getObjectById('image');
-        new Trigger(this.warrior, this.warrior);
-    }
->>>>>>> fbfca7e5cdb1ae39ff175d4924a82d7de6f568a9
 
     }
 }
 
 
-<<<<<<< HEAD
 core.registerBehaviourClass(ClickContainerBehaviour);
 core.registerBehaviourClass(GameStartupBehaviour);
-=======
-
-
-/* import { Quadtree } from "@timohausmann/quadtree-js";
-
-const myTree = new Quadtree({
-    x: 0,
-    y: 0,
-    width: 400,
-    height: 300
-}, 10, 4);
-
-
-const myObject = {
-    x: 200,
-    y: 100,
-    width: 35,
-    height: 70
-}
-
-myTree.insert(myObject);
-
- */
-core.registerBehaviourClass(PlayerManager);
->>>>>>> fbfca7e5cdb1ae39ff175d4924a82d7de6f568a9
